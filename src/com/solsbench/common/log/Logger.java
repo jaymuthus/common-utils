90b134afddd68abc6df4bc1dd8e0af75c5f6edc0
jaymuthus <jaymuthus@gmail.com>
2012-12-31 00:33:38 -0800

    Build file

--- src/com/solsbench/common/log/Logger.java
+++ src/com/solsbench/common/log/Logger.java
@@ -3,11 +3,15 @@ package com.solsbench.common.log;
 
 public class Logger {
 	
-	public static Logger getLogger(String className) {
+	public static Logger getLogger(Class className) {
 		return new Logger();
 	}
 	
-	public void info(){
-		
+	public void info(String message){
+		System.out.println(message);
+	}
+	
+	public void error(String message, Exception ex){
+		System.out.println(message);
 	}
 }
